b15171a42acb4f7f03ca7438a6a46851c5a8ac47
stuartgo <61937450+stuartgo@users.noreply.github.com>
2022-02-16 09:18:42 +0100

    coped example

--- app.py
+++ app.py
@@ -1,32 +1,11 @@
+import dash
+import dash_bootstrap_components as dbc
 
-from dash import Dash, html, dcc
-import plotly.express as px
-import pandas as pd
+# Toggle the themes at [dbc.themes.LUX]
+# The full list of available themes is:
+# CERULEAN, COSMO, CYBORG, DARKLY, FLATLY, JOURNAL, LITERA, LUMEN, LUX, MATERIA, MINTY, PULSE, SANDSTONE,
+# SIMPLEX, SKETCHY, SLATE, SOLAR, SPACELAB, SUPERHERO, UNITED, YETI.
+# https://dash-bootstrap-components.opensource.faculty.ai/docs/themes/
 
-app = Dash(__name__)
-
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
-
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for your data.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
-])
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.LUX])
+server = app.server
