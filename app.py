8c78f6ba71e92814b51d3d66af0364f2454cb3bb
stuartgo <61937450+stuartgo@users.noreply.github.com>
2022-03-11 10:14:26 +0100

    can toggle variable on map

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import pandas as pd
 from urllib.request import urlopen
 import json
 from data_prep import get_data
+import dash
+import plotly.graph_objects as go
 app = Dash(__name__)
 
 # assume you have a "long-form" data frame
@@ -14,32 +16,42 @@ with open('geojson_counties.json') as json_file:
 
 data=get_data()
 
-
-
 import plotly.express as px
 
-fig = px.choropleth_mapbox(data, geojson=counties, locations='fips', color='land_area (km^2)',
-                           color_continuous_scale="Viridis",
-                           range_color=(data['land_area (km^2)'].min(),data['land_area (km^2)'].max()),
-                           mapbox_style="carto-positron",
-                           zoom=3, center = {"lat": 37.0902, "lon": -95.7129},
-                           opacity=0.5,
-                           labels={'unemp':'unemployment rate'}
-                          )
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
+cols_dd=["male","female","population/2010","life-expectancy"]
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
 
     html.Div(children='''
         Dash: A web application framework for your data.
     '''),
-
+    dcc.Dropdown(
+        id='demo-dropdown',
+        options=[{'label': k, 'value': k} for k in cols_dd],
+        value=cols_dd[0]
+    ),
     dcc.Graph(
-        id='example-graph',
-        figure=fig
+        id='display-selected-values',
     )
 ])
+
+
+@app.callback(
+    dash.dependencies.Output('display-selected-values', 'figure'),
+    [dash.dependencies.Input('demo-dropdown', 'value')])
+def update_output(value):
+    print(value)
+    fig = px.choropleth_mapbox(data, geojson=counties, locations='fips', color=value,
+                           color_continuous_scale="Viridis",
+                           range_color=(data[value].min(),data[value].max()),
+                           mapbox_style="carto-positron",
+                           zoom=3, center = {"lat": 37.0902, "lon": -95.7129},
+                           opacity=0.5,
+                           labels={'unemp':'unemployment rate'}
+                          )
+    fig.update_layout(title=f"<b>{value}</b>", title_x=0.5)
+    return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
