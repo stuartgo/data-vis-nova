e953b61c03f9f0811e7a20ff51f65f07a8f2ad36
stuartgo <61937450+stuartgo@users.noreply.github.com>
2022-03-14 14:40:26 +0100

    rewrote some code and added county toggle

--- dvis_app.py
+++ dvis_app.py
@@ -5,11 +5,12 @@ import plotly.graph_objects as go
 import plotly.express as px
 import pandas as pd
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 from dash.dependencies import Input, Output
-
-from dvis_data import pres_states, pres_states_winners
+import json
+import dash_daq as daq
+from dvis_data import pres_states, pres_states_winners,pres_counties,counties,pres_county_winners
 ########
 
 # App layout
@@ -24,11 +25,17 @@ app.layout = html.Div([
         min = 2000, max = 2020, step = 4, value = 2020, # min, max, step, default of the slider
         id = "select_year", # unique id of the slider object
         marks = {
-            2000: {"label": 2000}, 2004: {"label": 2004},
-            2008: {"label": 2008}, 2012: {"label": 2012},
-            2016: {"label": 2016}, 2020: {"label": 2020}
+            2000: {"label": "2000"}, 2004: {"label": "2004"},
+            2008: {"label": "2008"}, 2012: {"label": "2012"},
+            2016: {"label": "2016"}, 2020: {"label": "2020"}
         },
         # vertical = 1 # uncomment to change slider to vertical orientation
+    ),
+    daq.ToggleSwitch(
+        id='state_county_toggle',
+        value=False,
+        label='State?',
+        labelPosition='bottom'
     ),
     html.Div(id = 'slider_output_container'),
     html.Br(),
@@ -36,35 +43,57 @@ app.layout = html.Div([
     dcc.Graph(id = "usa_map")
 ])
 
+
+
 @app.callback(
     Output(component_id = "usa_map", component_property = "figure"),
-    Input(component_id = "select_year", component_property = "value")
+    Input(component_id = "select_year", component_property = "value"),
+    Input(component_id = "state_county_toggle", component_property = "value")
 )
-def update_graph(value):
+
+
+def update_graph(year, state):
     # checking values and type
-    print(value)
-    print(type(value))
 
     # container = f"Results for the {value} elections"
 
     # filtering election data for current year
     pres_states_winners_copy = pres_states_winners.copy()
-    pres_states_winners_copy = pres_states_winners_copy[pres_states_winners_copy.year == value]
+    pres_states_winners_copy = pres_states_winners_copy[pres_states_winners_copy.year == year]
 
+
+    pres_county_winners_copy = pres_county_winners.copy()
+    pres_county_winners_copy = pres_county_winners_copy[pres_county_winners_copy.year == year]
+    print(pres_county_winners_copy)
+
+    print(state,year,type(year))
     # chloropleth
-    fig = px.choropleth(
-        data_frame = pres_states_winners_copy,
-        locationmode = "USA-states",
-        locations = "state_po",
-        scope = "usa",
-        color = "color",
-        hover_data = ["state"],
-        template = "plotly_dark"
-    )
-
+    if state:
+        fig = px.choropleth(
+            data_frame = pres_states_winners_copy,
+            locationmode = "USA-states",
+            locations = "state_po",
+            scope = "usa",
+            #color = "color",
+            hover_data = ["state"],
+            template = "plotly_dark"
+        )
+    else:
+        fig = px.choropleth(
+            data_frame = pres_county_winners_copy,
+            geojson=counties,
+            locations = "county_fips",
+            #color = "color",
+            scope = "usa",
+            hover_data = ["county_name","candidate"],
+            template = "plotly_dark"
+        )
     return fig
+
+
+    
 
 
 
 if __name__ == "__main__":
-    app.run_server(debug = False)
+    app.run_server(debug = True)
--- dvis_data.py
+++ dvis_data.py
@@ -3,6 +3,7 @@
 
 #### Import libraries/modules ####
 import pandas as pd
+import json
 ########
 
 def load_data():
@@ -18,6 +19,12 @@ def load_data():
     pres_counties = pd.read_csv("countypres_2000-2020.csv") # county-level presidential elections
     senate = pd.read_csv("1976-2020-senate.csv", encoding = 'latin1') # senate elections
     info_counties = pd.read_csv("counties.csv") # county-level information, includes coordinates
+
+        #fixes fips of counties
+    pres_counties.dropna(inplace=True)
+    pres_counties.county_fips=pres_counties.county_fips.astype(int)
+    pres_counties.county_fips=pres_counties.county_fips.apply(lambda x: str("0"*int((5-len(str(x))))+str(x)) if len(str(x))<5 else str(x))
+
 
     pres_states.drop(
         columns = ["office", "version", "notes", "state_cen", "state_ic", "party_simplified"],
@@ -39,32 +46,63 @@ pres_states, pres_counties, senate, info_counties = load_data()
 pres_states = pres_states[pres_states.year >= 1996]
 
 # Create dataframe, pres_states_winners, with most voted candidate per state per year
-pres_states_winners = pd.DataFrame()
-pres_states_grouped = pres_states.groupby(["year", "state"]).max("candidatevotes")
-for vote in pres_states_grouped.candidatevotes:
-    pres_states_winners = pres_states_winners.append(pres_states.query("candidatevotes == @vote"))
-pres_states_winners.reset_index(inplace = True, drop = True)
+pres_states_winners = pres_states.copy()
+pres_states_winners.sort_values(["year","state_fips","candidatevotes"],ascending=False,inplace=True)
+pres_states_winners.drop_duplicates(["state_fips","year"],inplace=True)
+
+
+# Create dataframe, pres_county_winners, with most voted candidate per state per year
+pres_county_winners = pres_counties.copy()
+pres_county_winners.sort_values(["year","county_fips","candidatevotes"],ascending=False,inplace=True)
+pres_county_winners.drop_duplicates(["county_fips","year"],inplace=True)
+
+#Find the previous winner for each county
+def previous_winner(row):
+    if row.name==len(pres_county_winners)-1:
+        return None
+    prev_row=pres_county_winners.iloc[row.name+1,:]
+    if prev_row.county_fips==row.county_fips:
+        return prev_row.party
+    else:
+        return None
+
+
+
+pres_county_winners.sort_values(["county_fips","year"],ascending=[True,False],inplace=True)
+pres_county_winners.reset_index(inplace=True)
+print(pres_county_winners)
+pres_county_winners["prev_winner"]=pres_county_winners.apply(lambda x: previous_winner(x) ,axis=1)
+
+print(pres_county_winners)
 
 # Add color code to pres_states_winners dataframe based on winning party
-color_code = []
-for index, year, party in zip(pres_states_winners.index, pres_states_winners.year, pres_states_winners.party_detailed):
-    # 51 states: subtracting 51 to current index retrieves winning party on that same state for the previous election
-    if year >= 2000: # prevents index out of range error for 1996 (there's no 1992 data)
-        prev_party = pres_states_winners.loc[index-51, "party_detailed"]
-        if (party == "REPUBLICAN") and (prev_party == "REPUBLICAN"):
-            # red if winning party was republican for two elections in a row
-            color_code.append("red")
-        if (party == "REPUBLICAN") and (prev_party != "REPUBLICAN"):
-            # red stripes if winning party changed from democrat to republican
-            color_code.append("red_stripes")
-        if (party == "DEMOCRAT") and (prev_party == "DEMOCRAT"):
-            # blue if winning party was democrat for two elections in a row
-            color_code.append("blue")
-        if (party == "DEMOCRAT") and (prev_party != "DEMOCRAT"):
-            # red stripes if winning party changed from republican to democrat
-            color_code.append("blue_stripes")      
-        if (party != "DEMOCRAT") and (party != "REPUBLICAN"):
-            # choose another color for non-dem/rep parties (there's only one other)
-            color_code.append("other")
-pres_states_winners = pres_states_winners[pres_states_winners.year >= 2000]
-pres_states_winners["color"] = color_code
+# color_code_state = []
+# for index, year, party in zip(pres_states_winners.index, pres_states_winners.year, pres_states_winners.party_detailed):
+#     # 51 states: subtracting 51 to current index retrieves winning party on that same state for the previous election
+#     if year >= 2000: # prevents index out of range error for 1996 (there's no 1992 data)
+#         prev_party = pres_states_winners.loc[index-51, "party_detailed"]
+#         if (party == "REPUBLICAN") and (prev_party == "REPUBLICAN"):
+#             # red if winning party was republican for two elections in a row
+#             color_code_state.append("red")
+#         if (party == "REPUBLICAN") and (prev_party != "REPUBLICAN"):
+#             # red stripes if winning party changed from democrat to republican
+#             color_code_state.append("red_stripes")
+#         if (party == "DEMOCRAT") and (prev_party == "DEMOCRAT"):
+#             # blue if winning party was democrat for two elections in a row
+#             color_code_state.append("blue")
+#         if (party == "DEMOCRAT") and (prev_party != "DEMOCRAT"):
+#             # red stripes if winning party changed from republican to democrat
+#             color_code_state.append("blue_stripes")      
+#         if (party != "DEMOCRAT") and (party != "REPUBLICAN"):
+#             # choose another color for non-dem/rep parties (there's only one other)
+#             color_code_state.append("other")
+# pres_states_winners = pres_states_winners[pres_states_winners.year >= 2000]
+# pres_states_winners["color"] = color_code_state
+
+
+
+#geojson is required when working with counties as its not built in
+with open('geojson_counties.json') as json_file:
+    counties = json.load(json_file)
+
+
