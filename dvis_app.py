d1531edc575a0a4ee91ce99f240f910710b3dc46
Nuno Melo <nbcmelo@gmail.com>
2022-03-16 17:49:52 +0000

    checklist

--- dvis_app.py
+++ dvis_app.py
@@ -41,6 +41,20 @@ app.layout = html.Div([
         label='State?',
         labelPosition='bottom'
     ),
+    dcc.Checklist([
+            "ALABAMA", "ALASKA", "ARIZONA","ARKANSAS", "CALIFORNIA", "COLORADO",
+            "CONNECTICUT", "DELAWARE", "DISTRICT OF COLUMBIA", "FLORIDA", "GEORGIA",
+            "HAWAII", "IDAHO", "ILLINOIS", "INDIANA", "IOWA", "KANSAS", "KENTUCKY",
+            "LOUISIANA", "MAINE", "MARYLAND", "MASSACHUSETTS", "MICHIGAN",
+            "MINNESOTA", "MISSISSIPPI", "MISSOURI", "MONTANA", "NEBRASKA",
+            "NEVADA", "NEW HAMPSHIRE", "NEW JERSEY", "NEW MEXICO", "NEW WORK",
+            "NORTH CAROLINA", "NORTH DAKOTA", "OHIO", "OKLAHOMA", "OREGON",
+            "PENNSYLVANIA", "RHODE ISLAND", "SOUTH CAROLINA", "SOUTH DAKOTA",
+            "TENNESSEE", "TEXAS", "UTAH", "VERMONT", "VIRGINIA", "WASHINGTON",
+            "WEST VIRGINIA", "WISCONSIN", "WYOMING"
+        ],
+        ["WASHINGTON"]
+    ),
     # html.Div(id = 'slider_output_container'),
     html.Br(),
 
